cc7fca9783dfee558493af3d278768a7510a4200
Leon Bentrup <leon@leonadi.de>
2017-07-28 14:08:47 +0200

    Prevent Food from spawning behind pause button

--- src/components/food.lua
+++ src/components/food.lua
@@ -9,7 +9,7 @@ function Food.replace()
 	repeat
 		Food.x = math.random(2, Board.width-2)
 		Food.y = math.random(2, Board.height-2)
-	until(Players[1].matrix[Food.x][Food.y].x + Players[1].matrix[Food.x][Food.y].y == 0 )
+	until(Players[1].matrix[Food.x][Food.y].x + Players[1].matrix[Food.x][Food.y].y == 0 and not (Food.x > Board.width - 4 and Food.y < 4))
 end
 
 function Food.draw()
